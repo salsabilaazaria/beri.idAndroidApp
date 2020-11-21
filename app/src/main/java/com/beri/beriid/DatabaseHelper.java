870f8b9a0bb5d94db38ade0f94d33b13690bb382
Salsabila Azaria <salsabilaazariaa@gmail.com>
2020-11-21 17:42:58 +0700

    addDatabase

---
+++ app/src/main/java/com/beri/beriid/DatabaseHelper.java
@@ -0,0 +1,78 @@
+package com.beri.beriid;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "beriiddatabase";
+    private static final int DB_VERSION = 1;
+
+    public DatabaseHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String sqlUsers = "CREATE TABLE users(id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "name TEXT, " +
+                "email TEXT, " +
+                "password TEXT, " +
+                "nik TEXT, " +
+                "address TEXT)";
+        String sqlProducts = "CREATE TABLE products(id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "name TEXT, " +
+                "description TEXT, " +
+                "quantity INTEGER)";
+        String sqlFoundations = "CREATE TABLE foundations(id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "name TEXT, " +
+                "description TEXT, " +
+                "address TEXT)";
+        String sqlDonations = "CREATE TABLE donations(id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "user_id INTEGER,product_id INTEGER,foundation_id INTEGER,"+
+                "FOREIGN KEY(user_id) REFERENCES users(id), " +
+                "FOREIGN KEY(product_id) REFERENCES products(id), " +
+                "FOREIGN KEY(foundation_id) REFERENCES foundations(id))";
+
+        sqLiteDatabase.execSQL(sqlUsers);
+        sqLiteDatabase.execSQL(sqlProducts);
+        sqLiteDatabase.execSQL(sqlFoundations);
+        sqLiteDatabase.execSQL(sqlDonations);
+
+    }
+
+
+
+    public boolean addData(String name, String description, String address) {
+
+        SQLiteDatabase db = getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("name",name);
+        contentValues.put("description", description);
+        contentValues.put("address", address);
+        db.insert("foundations", null, contentValues);
+
+        db.close();
+
+        return true;
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        String sqlUsers = "DROP TABLE IF EXISTS users";
+        String sqlProducts = "DROP TABLE IF EXISTS products";
+        String sqlFoundations = "DROP TABLE IF EXISTS foundations";
+        String sqlDonations = "DROP TABLE IF EXISTS donations";
+
+        sqLiteDatabase.execSQL(sqlUsers);
+        sqLiteDatabase.execSQL(sqlProducts);
+        sqLiteDatabase.execSQL(sqlFoundations);
+        sqLiteDatabase.execSQL(sqlDonations);
+
+        onCreate(sqLiteDatabase);
+    }
+}
+
--- app/src/main/java/com/beri/beriid/MainActivity.java
+++ app/src/main/java/com/beri/beriid/MainActivity.java
@@ -3,6 +3,7 @@ package com.beri.beriid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        DatabaseHelper db = new DatabaseHelper(this);
+
+//                db.addData("Sasana Tresna Werdha Ciracas","Nursing Home","Jl. Raya Ciracas No.60, RT.4/RW.11, Klp. Dua Wetan, Ciracas, Kota Jakarta Timur");
+//                db.addData("Elderly Nursing Kasih Ayah Bunda","Nursing Home","JL. Ternate Raya, Karawaci, Perumnas III, Bencongan Indah, Kec. Tangerang");
+//                db.addData("Marfati Nursing Home","Nursing Home","Jl. Dr. Sitanala, 85, Tangerang, 166, RT.001/RW.003, Mekarsari, Neglasari, Tangerang City");
+//                db.addData("Kidung Salomo Agape Elderly Home","Nursing Home","Jl. Utama I No.83, Pd. Karya, Kec. Pd. Aren, Kota Tangerang Selatan");
+//                db.addData("Elderly Nursing Jara Mara Pati","Nursing Home","JL. Seririt - Singaraja, Seririt, Buleleng, Banjar, Kec. Banjar, Kabupaten Buleleng");
+//                db.addData("Baitunnisa Foundation", "Orphanage", "Jl. Raya H. Abdullah No.30, Pakulonan Bar., Kec. Klp. Dua, Tangerang");
+//                db.addData("Al Mubaroh Foundation", "Orphanage", "Jl. KH Hasyim Ashari Rt.05, Gg. Jambu No.2, RT.006/RW.005, Buaran Indah, Kec. Tangerang, Kota Tangerang");
+//                db.addData("Pelangi Foundation", "Orphanage", "Cigondewah Hilir, Margaasih, Bandung");
+//                db.addData("Sayap Ibu Foundation", "Orphanage", "Jl. Graha Raya Bintaro No.33B, Pd. Kacang Bar., Kec. Pd. Aren, Kota Tangerang Selatan");
+//                db.addData("Rumah Yatim", "Orphanage", "Jl. Beringin Raya No.94, RT.002/RW.008, Karawaci Baru, Kec. Karawaci, Kota Tangerang");
+
+
+
+        Toast.makeText(this,"Value Saved", Toast.LENGTH_LONG).show();
+
     }
 }
